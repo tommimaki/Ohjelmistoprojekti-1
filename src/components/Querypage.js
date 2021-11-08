585879ec25030e931b57c2d11bedd552d30af411
Markus Sibakov <markus.sibakov@myy.haaga-helia.fi>
2021-11-08 16:53:55 +0200

    querypage

---
+++ src/components/Querypage.js
@@ -0,0 +1,26 @@
+import React, {useState, useEffect} from 'react';
+import Query from './Query';
+
+import Button from '@mui/material/Button';
+
+export default function QueryPage(){
+    const [queries, setQueries] = useState([]);
+
+    useEffect(() => {
+        setQueries ( [{question: "kysymys", questionType: "radio", answers: [1, 2, 3]}] );
+    }, []);
+
+
+    const renderQueries = () => {
+        queries.forEach(question => {
+            <Query query={question}/>
+        });
+    }
+
+    return (
+        <div>
+            {renderQueries}
+            <Button variant="contained">Send</Button>
+        </div>
+    );
+}
