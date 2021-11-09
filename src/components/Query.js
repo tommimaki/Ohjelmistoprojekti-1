e25c2de2fe52db7311fe373fc333d11b4450716d
tommimaki <bgp010@myy.haaga-helia.fi>
2021-11-09 10:20:09 +0200

    Query edit

--- src/components/Query.js
+++ src/components/Query.js
@@ -1,25 +1,31 @@
 import React from 'react';
 import question from './Querypage';
-
-import { FormControl, FormLabel, RadioGroup, Radio, FormControlLabel } from '@mui/material';
+import { border } from '@mui/system';
+import { FormControl, FormLabel, RadioGroup, Radio, FormControlLabel, Grid, Button } from '@mui/material';
 
 
 
 export default function Query(props) {
     return <div>
+        <Grid container spacing={4} alignItems="center"
+            justifyContent="center" style={{ minHeight: '220px' ,border: "1px solid grey"}} >
+            <Grid item>
 
-        <FormControl component="fieldset">
-            <FormLabel component="legend">question</FormLabel>
-            <RadioGroup
-                aria-label="kysymys"
-                defaultValue=""
-                name="radio-buttons-group"
-            >
-                <FormControlLabel value="1" control={<Radio />} label="1" />
-                <FormControlLabel value="2" control={<Radio />} label="2" />
-                <FormControlLabel value="3" control={<Radio />} label="3" />
-            </RadioGroup>
-        </FormControl>
+                <FormControl component="fieldset">
+                    <FormLabel component="legend">Question</FormLabel>
+                    <RadioGroup
+                        aria-label="kysymys"
+                        defaultValue=""
+                        name="radio-buttons-group"
+                    >
+                        <FormControlLabel value="1" control={<Radio />} label="1" />
+                        <FormControlLabel value="2" control={<Radio />} label="2" />
+                        <FormControlLabel value="3" control={<Radio />} label="3" />
+                        <Button variant="contained">Save answer</Button>
+                    </RadioGroup>
+                </FormControl>
 
+            </Grid>
+        </Grid>
     </div>;
 }
