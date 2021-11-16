5e21120762ed8fbc441d0d1fb4841103f65ab9ea
Markus Sibakov <markus.sibakov@myy.haaga-helia.fi>
2021-11-16 20:17:49 +0200

    querypage hakee kysymykset ja query on stateless

--- src/components/Query.js
+++ src/components/Query.js
@@ -1,72 +1,42 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import { TextField, FormControl, FormLabel, RadioGroup, Radio, FormControlLabel, Grid } from '@mui/material';
 
 
+//STATELESS COMPONENT
+export default function Query(props) {
 
-export default function Query() {
-
-
-    //api call
-    const [question, setQuestion] = useState([]);
-    const [loading, setLoading] = useState(true);
-
-    useEffect(() => {
-
-        async function loadQuestions() {
-
-            const response = await fetch("https://queryapp-backend.herokuapp.com/groups/2/questions");
-            const quest = await response.json();
-
-            setQuestion(quest)
-            setLoading(false);
-        }
-
-        loadQuestions();
-
-    }, []);
-    if (loading) return <h5>Questions loading...</h5>
-
-    //questions array
-    const questJsx = [];
-
-    //looppaa arrayn, tekee kysymyksen ja vastausvaihtoehdot
-    question.forEach(kys => {
-        questJsx.push(
-
-            <div key={kys.id}>
-                <Grid container spacing={4} alignItems="center"
-                    justifyContent="center" >
-                      <Grid item>  
-                    <FormControl component="fieldset">
-                        <FormLabel component="legend"> {kys.description} </FormLabel>
-                        <RadioGroup
-                            aria-label="kysymys"
-                            defaultValue=""
-                            name="radio-buttons-group"
-                        >
-                            <FormControlLabel value="1" control={<Radio />} label={kys.answers[0]} />
-                            <FormControlLabel value="2" control={<Radio />} label={kys.answers[1]} />
-                            <FormControlLabel value="3" control={<Radio />} label={kys.answers[2]} />
-                            <FormControlLabel value="4" control={<Radio />} label={kys.answers[3]} />
-
-                        </RadioGroup>
+    if(props.query.type === "radio"){
+       return (
+        <div key={props.query.id}>
+            <Grid container spacing={4} alignItems="center"
+                justifyContent="center" >
+                <Grid item>  
+                <FormControl component="fieldset">
+                    <FormLabel component="legend"> {props.query.description} </FormLabel>
+                    <RadioGroup
+                        aria-label="kysymys"
+                        defaultValue=""
+                        name="radio-buttons-group"
+                    >
+                        {
+                        props.query.answers.map(
+                            (answer, index) => 
+                            <FormControlLabel value={index} control={<Radio />} label={answer} />
+                        )
+                        }
+                    </RadioGroup>
 
 
-                    </FormControl>
-                    </Grid>
+                </FormControl>
                 </Grid>
-            </div>
-        );
+            </Grid>
+        </div>
+       ); 
+    }
 
-    });
-
-
-
-    return <div>
-        
-        {questJsx}
-        <TextField id="outlined-basic" label="Name" variant="outlined" />
-
-
-    </div>;
+    else return <div>Question type not supported</div>;
+    
 }
+
+
+
--- src/components/Querypage.js
+++ src/components/Querypage.js
@@ -1,30 +1,38 @@
 import React, {useState, useEffect} from 'react';
 import Query from './Query';
 
-
-
 import Button from '@mui/material/Button';
 
 export default function QueryPage(){
     const [queries, setQueries] = useState([]);
+    const [loaded, setLoaded] = useState(false);
 
     useEffect(() => {
-        setQueries ( [{question: "kysymys", questionType: "radio", answers: [1, 2, 3]}] );
+        //setQueries ( [{question: "kysymys", questionType: "radio", answers: [1, 2, 3]}] );
+        fetch('https://queryapp-backend.herokuapp.com/groups/2/questions')
+        .then( response => response.json() )
+        .then( data => {
+            setQueries(data);
+            setLoaded(true);
+        } )
+        .catch( err => console.error(err) );
     }, []);
 
-
-    const renderQueries = () => {
-        queries.forEach(question => {
-            <Query query={question}/>
-        });
+    const handleSendAnswers = () => {
+        //TODO kerää vastaukset json taulukkoon ja POSTaa backendiin
     }
 
-    return (
-        <div>
-            {/* {renderQueries} */}
-            
-            <Query /> 
-            <Button variant="contained">Send</Button>
-        </div>
-    );
+    if(!loaded){
+        return (
+            <div>Loading questions.</div>
+        );
+    } else {
+        return (
+            <div>
+                {queries.map((query, index) => <Query query={query}/>)}
+                <Button variant="contained" onClick={handleSendAnswers}>Send</Button>
+            </div>
+        );
+    }
+    
 }
