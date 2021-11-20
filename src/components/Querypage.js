6030b5c564f0407dd6b8e96c15bb1b6b51297c8e
Tommi Mäki <tommimaki@Tommi-MBP.fritz.box>
2021-11-20 17:37:07 +0100

    post, miten tää menee lol help

--- src/components/Querypage.js
+++ src/components/Querypage.js
@@ -1,38 +1,56 @@
-import React, {useState, useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 import Query from './Query';
 
 import Button from '@mui/material/Button';
 
-export default function QueryPage(){
+export default function QueryPage() {
     const [queries, setQueries] = useState([]);
     const [loaded, setLoaded] = useState(false);
 
     useEffect(() => {
         //setQueries ( [{question: "kysymys", questionType: "radio", answers: [1, 2, 3]}] );
         fetch('https://queryapp-backend.herokuapp.com/groups/2/questions')
-        .then( response => response.json() )
-        .then( data => {
-            setQueries(data);
-            setLoaded(true);
-        } )
-        .catch( err => console.error(err) );
+            .then(response => response.json())
+            .then(data => {
+                setQueries(data);
+                setLoaded(true);
+            })
+            .catch(err => console.error(err));
     }, []);
 
     const handleSendAnswers = () => {
         //TODO kerää vastaukset json taulukkoon ja POSTaa backendiin
+
+
+        const data = { nickname: 'testi', answers: query.answers };
+
+        fetch('https://queryapp-backend.herokuapp.com/groups/ID/answers', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(data),
+        })
+            .then(response => response.json())
+            .then(data => {
+                console.log('Success:', data);
+            })
+            .catch((error) => {
+                console.error('Error:', error);
+            });
     }
 
-    if(!loaded){
+    if (!loaded) {
         return (
             <div>Loading questions.</div>
         );
     } else {
         return (
             <div>
-                {queries.map((query, index) => <Query query={query}/>)}
+                {queries.map((query, index) => <Query query={query} />)}
                 <Button variant="contained" onClick={handleSendAnswers}>Send</Button>
             </div>
         );
     }
-    
+
 }
